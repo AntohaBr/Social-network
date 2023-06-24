5e4b736c187ff09f583504b73f941de571ff8557
AntohaBr <brelanton@gmail.com>
2023-06-24 22:33:58 +0300

    fix(login): refactor

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {LoginForm} from 'Components/Login'
 import {PATH} from 'Constants/Routing-constants'
-import {Navigate, useNavigate} from 'react-router-dom'
+import {Navigate} from 'react-router-dom'
 import {useAppSelector} from 'Utils'
 import {selectIsAuth} from 'Store/Selectors'
 
